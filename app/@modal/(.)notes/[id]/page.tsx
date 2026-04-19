3baac6dc0f03aec0aa48ed4670213431d593652f
Vega <alexkarpytskyi@gmail.com>
2026-04-19 23:43:07 +0300

    Add modal intercepting route via git

---
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -0,0 +1,11 @@
+import NotePreview from '@/components/NotePreview/NotePreview';
+
+interface Props {
+  params: Promise<{ id: string }>;
+}
+
+export default async function NoteModalPage({ params }: Props) {
+  const { id } = await params;
+  return <NotePreview id={id} />;
+}
+
